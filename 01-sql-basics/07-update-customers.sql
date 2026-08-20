10454f6bd650f68d498bf41a09b91def251c98ac
Luan Souza <311944319+LuanSouzaData@users.noreply.github.com>
2026-08-19 23:48:28 -0300

    feat: add customer city data

---
+++ 01-sql-basics/07-update-customers.sql
@@ -0,0 +1,20 @@
+-- Update customer cities
+UPDATE customers
+SET city = 'Sao Paulo'
+WHERE customer_id = 1;
+
+UPDATE customers
+SET city = 'Rio de Janeiro'
+WHERE customer_id = 2;
+
+UPDATE customers
+SET city = 'Sao Paulo'
+WHERE customer_id = 3;
+
+UPDATE customers
+SET city = 'Belo Horizonte'
+WHERE customer_id = 4;
+
+UPDATE customers
+SET city = 'Rio de Janeiro'
+WHERE customer_id = 5;
